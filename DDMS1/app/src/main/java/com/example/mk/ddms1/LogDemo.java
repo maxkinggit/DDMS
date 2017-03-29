2352d7043a98be9a5dcc5a3c53eb72a4795c65ab
maxkinggit <497559335@qq.com>
2017-03-29 16:10:43 +0800

    DDMS

---
+++ DDMS1/app/src/main/java/com/example/mk/ddms1/LogDemo.java
@@ -0,0 +1,28 @@
+package com.example.mk.ddms1;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+public class LogDemo extends Activity {
+    private static final String ACTIVITY_TAG="LogDemo";
+    private Button bt;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (Button)findViewById(R.id.bt);
+        bt.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v) {
+                Log.v(LogDemo.ACTIVITY_TAG, "This is Verbose.");
+                Log.d(LogDemo.ACTIVITY_TAG, "This is Debug.");
+                Log.i(LogDemo.ACTIVITY_TAG, "This is Information");
+                Log.w(LogDemo.ACTIVITY_TAG, "This is Warnning.");
+                Log.e(LogDemo.ACTIVITY_TAG, "This is Error.");
+            }
+        });
+    }
+}
+
